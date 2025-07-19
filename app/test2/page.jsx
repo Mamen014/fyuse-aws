ff17bda3c1b79aa0ec5bc9ac0c3420c905ef8dce
Mamen014 <ryaniaska14@gmail.com>
2025-07-20 01:05:35 +0700

    fix &apos;

--- app/test2/page.jsx
+++ app/test2/page.jsx
@@ -17,7 +17,7 @@ export default function ShareCanvas() {
           />
         </div>
         <h1 className="text-[54px] text-primary font-bold leading-tight mb-2">
-          Ryan's Perfect Look
+          Ryan&apos;s Perfect Look
         </h1>
       </div>
 
